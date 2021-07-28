9add73d9d5d5bfdfda5fdab90c390adf7a0cefae
soongon <soongon@hucloud.co.kr>
2021-07-28 14:01:17 +0900

    initial commit

---
+++ src/main/java/kr/re/kitri/posts/model/Post.java
@@ -0,0 +1,37 @@
+package kr.re.kitri.posts.model;
+
+public class Post {
+    private long id;
+    private String title;
+    private String body;
+    private long likes;
+
+    public Post() {}
+    public Post(long id, String title, String body, long likes) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.likes = likes;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getBody() {
+        return body;
+    }
+    public long getLikes() {
+        return likes;
+    }
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                ", likes=" + likes +
+                '}';
+    }
+}
