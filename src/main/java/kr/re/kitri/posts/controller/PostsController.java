900c63db0b65ad143c19af7e3048c91a3e36b658
soongon <soongon@hucloud.co.kr>
2021-07-28 11:55:07 +0900

    initial commit

--- src/main/java/kr/re/kitri/posts/controller/PostsController.java
+++ src/main/java/kr/re/kitri/posts/controller/PostsController.java
@@ -1,13 +1,18 @@
 package kr.re.kitri.posts.controller;
 
+import kr.re.kitri.posts.service.PostsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostsController {
 
+    @Autowired
+    private PostsService postsService;
+
     @GetMapping("/posts")
     public String getAllPosts() {
-        return "글 전체보기..";
+        return postsService.viewAllPosts();
     }
 }
