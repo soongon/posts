781cab8c51aebae3c64e17c45b19a9e1c0cfa7b9
soongon <soongon@hucloud.co.kr>
2021-07-29 17:22:46 +0900

    initial commit

--- src/main/java/kr/re/kitri/posts/service/PostsServiceImpl.java
+++ src/main/java/kr/re/kitri/posts/service/PostsServiceImpl.java
@@ -2,6 +2,8 @@ package kr.re.kitri.posts.service;
 
 import kr.re.kitri.posts.dao.PostsRepository;
 import kr.re.kitri.posts.model.Post;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +12,14 @@ import java.util.List;
 @Service
 public class PostsServiceImpl implements PostsService {
 
+    public static Logger logger =
+            LoggerFactory.getLogger(PostsServiceImpl.class);
+
     @Autowired
     private PostsRepository postsRepository;
 
     @Override public List<Post> viewAllPosts() {
-        // DB에서 글을 가지고 온다.
+        logger.info("서비스 호출......");
         return postsRepository.selectPostsAll();
     }
     @Override public Post viewPostByPostId(long postId) {
